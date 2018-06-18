8f2993bad7d97ae0814727fe46ee07f2fa02fcbf
Lucas Massazza <lucasmassazza@gmail.com>
2018-06-18 20:48:46 -0300

    Realizacion de Ejercicios de Prueba Mayo

---
+++ src/main/java/Repaso_Fecha_5_27_2018/PruebaPreviaBarbutto_7_10_2018/Ejercicio1/Compactar.java
@@ -0,0 +1,12 @@
+package Repaso_Fecha_5_27_2018.PruebaPreviaBarbutto_7_10_2018.Ejercicio1;
+
+public class Compactar {
+
+    public int[] compactar(int[] array){
+
+
+
+        return null;
+    }
+
+}
